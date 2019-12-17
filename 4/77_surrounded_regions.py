d30ecdae48e4f41459b7dfbcbb50797de4498637
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-16 18:40:46 -0800

    Update 77_surrounded_regions.py

--- 4/77_surrounded_regions.py
+++ 4/77_surrounded_regions.py
@@ -3,7 +3,7 @@ class Solution:
     @param: board: board a 2D board containing 'X' and 'O'
     @return: nothing
     """
-    def surroundedRegions(self, board):
+    def _surroundedRegions(self, board):
         # write your code here
         if board == []:
             return []
@@ -41,8 +41,52 @@ class Solution:
             board[i] = board[i][0:j]+'X'+board[i][j+1:]
             # board[i][j] = 'X'
 
+    def surroundedRegions(self, board):
+        # write your code here
+        if board == []:
+            return []
+        m = len(board)
+        n = len(board[0])
+        visited = set()
+        edge = set()
+        for i in range(m):
+            if i == 0 or i == m-1:
+                for j in range(n):
+                    if board[i][j] == 'O':
+                        edge.add((i,j))
+            else:
+                if board[i][0] == 'O':
+                    edge.add((i,0))
+                if board[i][n-1] == 'O':
+                    edge.add((i,n-1))
+        q = list(edge)
+        edge_visited = set()
+        while len(q) != 0:
+            i, j = q.pop()
+            directions = [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
+            visited.add((i,j))
+            for a, b in directions:
+                if (a,b) in visited:
+                    continue
+                if a>=0 and b>=0 and a<=m-1 and b<=n-1:
+                    if board[a][b] == 'O':
+                        # visited.add((a,b))
+                        q[:0] = [(a,b)]
+                        # if (a,b) in edge:
+                        #     edge_visited.add((a,b))
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    if (i,j) in visited:
+                        pass
+                    else:
+                        # board[i][j] = 'X'
+                        board[i] = board[i][0:j]+'X'+board[i][j+1:]
+        print(visited)
+        return
+
 if __name__ == "__main__":
-    x = ["XXXX","XOOX","XOOX","XOXX"]
+    x = ["XXXX","XOOX","XOOX","XXXX"]
     Solution().surroundedRegions(x)
     for e in x:
         print(e)
