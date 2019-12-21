ec8e3fa33405cc00ffb7f62b476f0c6b7812b961
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-20 20:45:17 -0800

    Create 661_convert_BST.py

---
+++ 5/661_convert_BST.py
@@ -0,0 +1,70 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: the new root
+    """
+    def convertBST(self, root):
+        # write your code here
+        
+        def flat_tree(root):
+            if root == None:
+                return
+            flat_tree(root.left)
+            sort_tree.append(root.val)
+            flat_tree(root.right)
+        def unflat_tree(root):
+            if root == None:
+                return
+            unflat_tree(root.left)
+            # sort_tree.append(root.val)
+            root.val = new_tree[self.index]
+            self.index += 1
+            unflat_tree(root.right)
+        def change_val(root):
+            if root == None:
+                return
+            for i in range(len(sort_tree)):
+                if sort_tree[i] >= root.val:
+                # if root.val < sort_tree[i]:
+                    temp = 0
+                    for j in range(i,len(sort_tree)):
+                        temp += sort_tree[j]
+                    root.val = temp
+                    break
+            change_val(root.left)
+            change_val(root.right)
+        
+        sort_tree = []
+        new_tree = []
+        flat_tree(root)
+        for i in range(len(sort_tree)):
+            temp = 0
+            for j in range(i,len(sort_tree)):
+                temp += sort_tree[j]
+            new_tree.append(temp)
+        self.index = 0
+        unflat_tree(root)
+        # change_val(root)
+        return root
+        
+if __name__ == "__main__":
+    root = TreeNode(5)
+    root.left = TreeNode(2)
+    root.right = TreeNode(13)
+    Solution().convertBST(root)
+    print(root.val)
+
+    
