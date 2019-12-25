dfa66094a837087972292fc2a0efba9ebc1771c7
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-24 23:46:09 -0800

    Create 612_k_closest_points.py

---
+++ option_1/612_k_closest_points.py
@@ -0,0 +1,18 @@
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+
+class Solution:
+    """
+    @param points: a list of points
+    @param origin: a point
+    @param k: An integer
+    @return: the k closest points
+    """
+    def kClosest(self, points, origin, k):
+        # write your code here
+        return sorted(points,key=lambda point: (((point.x-origin.x)**2+(point.y-origin.y)**2),point.x,point.y))[:k]
