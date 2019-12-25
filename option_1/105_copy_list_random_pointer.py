0c5486ef744df59abe24c33b1b0e42f3a7aa222a
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-25 15:24:26 -0800

    Create 105_copy_list_random_pointer.py

---
+++ option_1/105_copy_list_random_pointer.py
@@ -0,0 +1,15 @@
+"""
+Definition for singly-linked list with a random pointer.
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+"""
+
+
+class Solution:
+    # @param head: A RandomListNode
+    # @return: A RandomListNode
+    def copyRandomList(self, head):
+        # write your code here
