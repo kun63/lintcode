a9009c5b80c9482aca3a81988e2ec71c9abb9f77
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-25 16:07:06 -0800

    Update 105_copy_list_random_pointer.py

--- option_1/105_copy_list_random_pointer.py
+++ option_1/105_copy_list_random_pointer.py
@@ -13,3 +13,25 @@ class Solution:
     # @return: A RandomListNode
     def copyRandomList(self, head):
         # write your code here
+        curr = head
+        new_head = None
+        while curr:
+            
+            temp = RandomListNode(curr.label)
+            if new_head == None:
+                new_head = temp
+            if pre:
+                pre.next = temp
+            pre = temp
+            curr.random = (curr.random, temp)
+            # change = curr
+            curr = curr.next
+            # change.next = temp
+        curr = head
+        curr_new = new_head
+        while curr:
+            if not curr.random[0]:
+                curr_new.random = curr.random[0].random[1]
+            curr = curr.next
+            curr_new = curr_new.next
+        return new_head
