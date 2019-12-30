0a234d92d54d50e665cc5acf6dbc1d806db31429
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-30 13:04:07 -0800

    Update 629_minunum_spanning_tree.py

--- option_1/629_minunum_spanning_tree.py
+++ option_1/629_minunum_spanning_tree.py
@@ -5,21 +5,73 @@ class Connection:
     def __init__(self, city1, city2, cost):
         self.city1, self.city2, self.cost = city1, city2, cost
 '''
+class Connection:
+
+    def __init__(self, city1, city2, cost):
+        self.city1, self.city2, self.cost = city1, city2, cost
+
+
 class Solution:
     # @param {Connection[]} connections given a list of connections
     # include two cities and cost
     # @return {Connection[]} a list of connections from results
     def lowestCost(self, connections):
         # Write your code here
-        import collections
-        self.edges = collections.defaultdict(set)
-        self.best = []
-        self.allcity = set()
+        connections.sort(key = lambda c: (c.cost, c.city1, c.city2))
+        connections.reverse()
+        city_set = []
+        allcity = set()
+        trash = []
         for c in connections:
-            c1, c2, w  = c.city1, c.city2, c.cost
-            self.edges[c1].add((c2,w))
-            self.edges[c2].add((c1,w))
-        def dfs(visited,curr,cost):
+            c1, c2, cost = c.city1, c.city2, c.cost
+            allcity.add(c1)
+            allcity.add(c2)
+            if c1 == c2:
+                trash.append(c)
+        for t in trash:
+            connections.remove(t)
+        outcome = []
+        
+        while connections:
+            c = connections.pop()
+            c1, c2, cost = c.city1, c.city2, c.cost
+            flag = 0
+            if city_set == []:
+                city_set.append({c1,c2})
+                outcome.append(c)
+                if city_set[0] == allcity:
+                    return outcome
+                continue
+            for s in city_set:
+                if c1 in s or c2 in s:
+                    flag = 1
+                    if c2 in s:
+                        c1, c2 = c2, c1
+                    if c2 in s:
+                        break
+                    else:
+                        for t in city_set:
+                            if c2 in t:
+                                s.update(t)
+                                city_set.remove(t)
+                                break
+                        s.add(c2)
+                        outcome.append(c)
+                        break
+            if flag == 0:
+                city_set.append({c1,c2})
+                outcome.append(c)
+            if city_set[0] == allcity:
+                    return outcome
             
+        return []
 
-        dfs({connections[0].city1},connections[0].city1,0)
+
+if __name__ == "__main__":
+    connections = []
+    connections.append(Connection("Acity","Bcity",1))
+    connections.append(Connection("Acity","Ccity",2))
+    connections.append(Connection("Bcity","Ccity",3))
+    connections = Solution().lowestCost(connections)
+    for c in connections:
+        print(c.city1,c.city2,c.cost)
