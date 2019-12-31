f878200960b74f9748f536ea5fffda6ba9553bfc
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-31 10:44:12 -0800

    Update 384_longest_substring_without_repeat_character.py

--- option_3/384_longest_substring_without_repeat_character.py
+++ option_3/384_longest_substring_without_repeat_character.py
@@ -5,23 +5,23 @@ class Solution:
     """
     def lengthOfLongestSubstring(self, s):
         # write your code here
-        import collections
+        # import collections
         if len(s) < 2:
             return len(s)
         start = 0
         end = 0
-        curr = collections.deque()
+        # curr = collections.deque()
+        curr = set()
         longest = 0
         while end <= len(s) - 1:
             while end <= len(s) - 1 and s[end] not in curr:
-                curr.append(s[end])
+                curr.add(s[end])
                 if len(curr) > longest:
                     longest = len(curr)
                 if end <= len(s)-1:
                     end += 1
-            curr.popleft()
-            # if start < end:
-            #     start += 1
+            curr.remove(s[start])
+            start += 1
 
         return longest
 
