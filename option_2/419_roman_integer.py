e3af309a6417f9b8182b978a41d97e00adf24c17
Kun Chen <58652154+kun63@users.noreply.github.com>
2019-12-30 23:45:54 -0800

    Update 419_roman_integer.py

--- option_2/419_roman_integer.py
+++ option_2/419_roman_integer.py
@@ -3,5 +3,37 @@ class Solution:
     @param s: Roman representation
     @return: an integer
     """
-    def romanToInt(self, s):
+    def _romanToInt(self, s):
+    
         # write your code here
+        if s == '':
+            return 0
+        roman = [['IX','VIII','VII','VI','V','VI','III','II','I'],['XC','LXXX','LXX','LX','L','XL','XXX','XX','X'],['CM','DCCC','DCC','DC','D','CD','CCC','CC','C'],['MMM','MM','M']]
+        index = 0
+        result = 0
+        s = s[::-1]
+        while s:
+            for i, t in enumerate(roman[index]):
+                if s.find(t[::-1]) == 0:
+                    s = s[len(t):]
+                    result += (9-i)*(10**index)
+                    
+                    break
+            index += 1
+        return result
+    def romanToInt(self, s):
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        result = 0
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[i+1]]:
+                result -= roman[s[i]]
+            else:
+                result += roman[s[i]]
+        result += roman[s[i+1]]
+        return result
+
+if __name__ == "__main__":
+    print(Solution().romanToInt('IV'))
+
+
+
