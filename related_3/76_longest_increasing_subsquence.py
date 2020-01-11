ad83e4121b97ab3adc9dd2cf1b7f1b0453f78b5b
Kun Chen <58652154+kun63@users.noreply.github.com>
2020-01-11 15:44:05 -0800

    Update 76_longest_increasing_subsquence.py

--- related_3/76_longest_increasing_subsquence.py
+++ related_3/76_longest_increasing_subsquence.py
@@ -18,7 +18,7 @@ class Solution:
                 len_longest = 1    
         result = max(result,len_longest)
         return result
-    def longestIncreasingSubsequence(self, nums):
+    def _longestIncreasingSubsequence(self, nums):
         l = len(nums)
         result = []
         for i in range(l):
@@ -31,6 +31,25 @@ class Solution:
             result.append(len(temp))
         # result = [len([x for x in nums[i:len(nums)+1] if x >= nums[i]]) for i in range(len(nums))]
         return max(result)
+    def longestIncreasingSubsequence(self, nums):
+        longest = 0
+        dp = {}
+        for i in range(len(nums)):
+            temp = []
+            if i == 0:
+                dp[i] = 1
+            else:
+                temp = [dp[j] for j in range(i) if nums[j] < nums[i]]
+                if temp == []:
+                    dp[i] = 1
+                else:
+                    dp[i] = max(temp) + 1
+            # print(i,dp[i])
+            # print(temp)
+            if dp[i] > longest:
+                longest = dp[i]
+
+        return longest
 
 
 
